c72898758e75a82d454be25d5444e21c3da24dac
Worawit Penglam <82023896+PoonwannadoCoding@users.noreply.github.com>
2024-05-07 21:28:57 +0700

    add button on the project for query the year

--- src/widget/Projects.tsx
+++ src/widget/Projects.tsx
@@ -1,13 +1,48 @@
 import './widget.css';
 import Reveal from './components/animation/RevealAnimation';
+import GitHubCalendar from 'react-github-calendar';
+import React, { useState } from 'react';
+import Button from './components/util/Button';
+import Grid from '@mui/material/Grid';
+
+
+
 
 export default function Porject(){
+
+    const yearList:number[] = [2021,2022,2023,2024];
+
+    const [curentYear, setCurrentYear] = useState<number>(yearList[yearList.length-1]);
+
     return(
         <div className="ProjectsContainer">
             <Reveal 
             children={
                 <h1 className="TextBoxHeader"> Projects </h1>
             } />
+            <Reveal children={
+                <GitHubCalendar 
+                colorScheme='dark'
+                username="PoonwannadoCoding"
+                //transformData={selectCurrentFullYear} 
+                year={curentYear}
+                labels={{
+                    totalCount: '{{count}} contributions in this year',
+                  }}
+                />
+            }>
+                
+            </Reveal>
+            <Grid container spacing={2} justifyContent="center"> {/* Centering the buttons */}
+                {yearList.map((year, index) => (
+                    <Grid item key={index}>
+                        <Button name={String(year)} onclick={() => setCurrentYear(year)} />
+                    </Grid>
+                ))}
+            </Grid>
+                
+              
+            
             
         </div>
     );
